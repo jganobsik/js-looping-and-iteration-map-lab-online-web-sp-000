74a2618d259c67eb5cc9db6bb5d71cf2a788987b
Joe Ganobsik <jganobsik@gmail.com>
2019-02-13 16:07:31 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -6,3 +6,18 @@ return list.map(function(driver) {
     return driver.toLowerCase();
   });
 }
+
+function nameToAttributes(list) {
+  return list.map(function(driver) {
+    const first = driver.split(' ')[0];
+    const last = driver.split(' ')[1];
+
+    return { firstName: frst, lastName: last };
+  });
+}
+
+function attributesToPhrase(drivers) {
+  return drivers.map(function(driver) {
+    return `${driver.name} is from ${driver.hometown}`;
+  });
+}
